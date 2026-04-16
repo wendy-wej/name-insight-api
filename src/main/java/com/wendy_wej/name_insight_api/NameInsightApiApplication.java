3ab4ed85bd242c0bf23635a6efb34362077a9f69
wendy-wej <wendu.nweje@gmail.com>
2026-04-16 22:49:29 +0100

    chore: project setup

---
+++ src/main/java/com/wendy_wej/name_insight_api/NameInsightApiApplication.java
@@ -0,0 +1,13 @@
+package com.wendy_wej.name_insight_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameInsightApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NameInsightApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wendy_wej/name_insight_api/NameInsightApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wendy_wej.name_insight_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NameInsightApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
